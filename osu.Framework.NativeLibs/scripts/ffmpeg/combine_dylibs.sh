b58bdb51be32f66d5f5bf53fd7a707232a7034cc
FreezyLemon <contact@freezylemon.com>
2023-08-23 00:32:49 +0200

    Upgrade combine_dylibs

--- osu.Framework.NativeLibs/scripts/ffmpeg/combine_dylibs.sh
+++ osu.Framework.NativeLibs/scripts/ffmpeg/combine_dylibs.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
 
+pushd . > /dev/null
 mkdir -p macOS-universal
-
-lipo -create macOS-arm64/libavcodec.58.dylib macOS-x86_64/libavcodec.58.dylib -output macOS-universal/libavcodec.58.dylib
-lipo -create macOS-arm64/libavformat.58.dylib macOS-x86_64/libavformat.58.dylib -output macOS-universal/libavformat.58.dylib
-lipo -create macOS-arm64/libavutil.56.dylib macOS-x86_64/libavutil.56.dylib -output macOS-universal/libavutil.56.dylib
-lipo -create macOS-arm64/libswscale.5.dylib macOS-x86_64/libswscale.5.dylib -output macOS-universal/libswscale.5.dylib
+cd macOS-arm64
+for lib_arm in *.dylib; do
+    lib_x86="../macOS-x86_64/$lib_arm"
+    
+    echo "-> Creating universal $lib_arm..."
+    lipo -create "$lib_arm" "$lib_x86" -output "../macOS-universal/$lib_arm"
+done
+popd > /dev/null
