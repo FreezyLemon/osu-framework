8ceddec0f2151a961e963b0346b2804d80c215dd
FreezyLemon <contact@freezylemon.com>
2023-08-28 14:37:43 +0200

    Allow linux-aarch64
    No VAAPI/VDPAU on aarch64, at least for now.

--- osu.Framework.NativeLibs/scripts/ffmpeg/build-linux.sh
+++ osu.Framework.NativeLibs/scripts/ffmpeg/build-linux.sh
@@ -6,26 +6,46 @@ SCRIPT_PATH=$(pwd)
 popd > /dev/null
 source "$SCRIPT_PATH/common.sh"
 
-FFMPEG_FLAGS+=(
-    --enable-vaapi
-    --enable-vdpau
-    --enable-hwaccel='h264_vaapi,h264_vdpau'
-    --enable-hwaccel='hevc_vaapi,hevc_vdpau'
-    --enable-hwaccel='vp8_vaapi,vp8_vdpau'
-    --enable-hwaccel='vp9_vaapi,vp9_vdpau'
+if [ -z "${arch-}" ]; then
+    PS3='Build for which arch? '
+    select arch in "x64" "aarch64"; do
+        if [ -z "$arch" ]; then
+            echo "invalid option"
+        else
+            break
+        fi
+    done
+fi
 
+if [ "$arch" == "aarch64" ]; then
+    FFMPEG_FLAGS+=(
+        --enable-cross-compile
+        --arch=aarch64
+        --cross-prefix=aarch64-linux-gnu-
+    )
+elif [ "$arch" == "x64" ]; then
+    FFMPEG_FLAGS+=(
+        --enable-vaapi
+        --enable-vdpau
+        --enable-hwaccel='h264_vaapi,h264_vdpau'
+        --enable-hwaccel='hevc_vaapi,hevc_vdpau'
+        --enable-hwaccel='vp8_vaapi,vp8_vdpau'
+        --enable-hwaccel='vp9_vaapi,vp9_vdpau'
+    )
+fi
+
+FFMPEG_FLAGS+=(
     --target-os=linux
 )
 
 pushd . > /dev/null
-prep_ffmpeg linux-x64
+prep_ffmpeg "linux-$arch"
 build_ffmpeg
 popd > /dev/null
 
-# gcc creates multiple symlinks per .so file for versioning.
-# We want to delete the symlinks to prevent weird behaviour with GitHub actions.
-rm linux-x64/*.so
-for f in linux-x64/*.so.*.*.*; do
+# Delete symlinked libraries to prevent weird behaviour with GitHub actions
+find "linux-$arch" -type l -delete
+# Rename library.so.A.B.C to library.so
+for f in "linux-$arch"/*.so.*.*.*; do
     mv -v "$f" "${f%.*.*.*}"
 done
-rm linux-x64/*.so.*
